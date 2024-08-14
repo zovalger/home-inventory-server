e5c0d63e575951381238515eec3d7741cb971e52
zovalger <zovalger@gmail.com>
2024-08-14 15:20:19 -0400

    setup test

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -4,15 +4,23 @@ import { CreateUserEmailDto, SendEmailInviteUserDto } from './dto';
 
 import { EmailTemplates } from './providers/email-templates';
 import { EmailSender } from './providers/email-sender';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EmailService {
   constructor(
+    private readonly configService: ConfigService,
     private readonly emailSender: EmailSender,
     private readonly emailTemplates: EmailTemplates,
   ) {}
 
+  isTestingMode() {
+    return this.configService.get('NODE_ENV') == 'test';
+  }
+
   async sendEmail_CreateUser(createUserEmailDto: CreateUserEmailDto) {
+    if (this.isTestingMode()) return;
+
     const { user } = createUserEmailDto;
 
     const { subject, html } =
@@ -22,6 +30,8 @@ export class EmailService {
   }
 
   async sendEmail_InviteUsers(SendEmailInviteUserDto: SendEmailInviteUserDto) {
+    if (this.isTestingMode()) return;
+
     const { familyName, createByUserName, invitations } =
       SendEmailInviteUserDto;
 
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { Server } from 'http';
+import { Repository } from 'typeorm';
+import { User, UserVerificationCode } from './../src/auth/entities';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { APP_PIPE } from '@nestjs/core';
 
-describe('AppController (e2e)', () => {
+describe('App (e2e)', () => {
   let app: INestApplication;
+  let server: Server;
+  let userRepository: Repository<User>;
+  let userVerificationCodeRepository: Repository<UserVerificationCode>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [
+        {
+          provide: APP_PIPE,
+          useValue: new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+          }),
+        },
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    userVerificationCodeRepository = moduleFixture.get<
+      Repository<UserVerificationCode>
+    >(getRepositoryToken(UserVerificationCode));
+
+    userRepository = moduleFixture.get<Repository<User>>(
+      getRepositoryToken(User),
+    );
+
     await app.init();
+
+    server = app.getHttpServer();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  // it('login', () => {
+  //   return request(server).get('/').expect(200).expect('Hello World!');
+  // });
+
+  describe('AuthModule (e2e)', () => {
+    // crear cuenta
+    const userData = {
+      name: 'zovalger',
+      lastName: 'dev',
+      email: 'zovalger',
+      password: 'Ab123456.',
+
+      // birthday: new Date('30-04-2002').toDateString(),
+    };
+
+    describe('create user account', () => {
+      // crear correctamente la cuenta
+      it('should create user account successfull', async () => {
+        const res = await request(server).post('/auth/register').send(userData);
+        // .expect(200);
+
+        console.log(res.body);
+
+        // expect(res.body).toMatchObject(User);
+      });
+
+      // contraseña debil
+
+      // dato mal
+    });
+
+    // contraseña o usuario incorrecta
+    // login de cuenta
+    // obtener perfil
+    // usuario no encontrado
   });
 });
